4c20a255e16ecbecd9bd433af6c27fedf5108332
elisemykle <44864422+elisemykle@users.noreply.github.com>
2020-03-16 14:23:38 +0100

    Uploaded new files

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,5 +1,20 @@
-import React from "react";
-import App from './App.js';
-import HomeContent from './HomeContent.js';
+import React from 'React';
+import Heading from './Heading.js';
 import NewsList from './NewsList.js';
-import LoginForm from './LoginForm.js';
+
+ function NewsList(props) {
+    return (
+
+        <div className="NewsList">
+        <Heading content="News"/>
+        <ul>
+            <li>Example 1</li>
+            <li>Example 2</li>
+            <li>Example 3</li>
+        </ul>
+        </div>
+
+    );
+}
+
+export default NewsList;
